750f1a7896bfcd3b12707b5a679b566f2b125c0e
Firas Chaabani <firaschaabani1@gmail.com>
2019-12-22 23:31:22 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,14 +2,14 @@ def second_challenge
   epic_tragedy = {
     :montague => {
 :patriarch => key1 ,
-:matriarch => key2, 
+:matriarch => key2,
 :hero => key3,
 :hero_friends => key4
       #your key/value pairs here!
-      }, 
+      },
     :capulet => {
       #your key/value pairs here!
       }}
 
-  
+
 end
